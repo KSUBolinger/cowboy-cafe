1635d0bbbc48a21c80e3c34147a7d4b1552fdbe5
Easton Bolinger <bolinger@ksu.edu>
2020-01-29 20:55:16 -0600

    Intial creation of data classes and completion of Rustler's Rib

---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class RustlersRibs
+    {
+        /// <summary>
+        /// The price for the ribs
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return 7.50;
+            }
+        }
+
+        /// <summary>
+        /// The calories for the ribs
+        /// </summary>
+        public uint Calories
+        {
+            get
+            {
+                return 894;
+            }
+        }
+
+        /// <summary>
+        /// Special instructions for preparing the ribs
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+
+                return instructions;
+            }
+        }
+    }
+}
