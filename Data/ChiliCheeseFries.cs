c9a9de9ca17ae634360f1bdd59e2f9ddaaf2dc71
Easton Bolinger <bolinger@ksu.edu>
2020-02-07 14:50:57 -0600

    Start of Milestone 2

---
+++ Data/ChiliCheeseFries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class ChiliCheeseFries
+    {
+        public double Price
+        {
+            get
+            {
+
+            }
+        }
+    }
+}
