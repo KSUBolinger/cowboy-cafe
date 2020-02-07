ea134bd1eff4ca31c0fd96e8cd4f4b67b15347ae
Easton Bolinger <bolinger@ksu.edu>
2020-02-07 15:00:29 -0600

    ChiliCheeseFries class started

--- Data/ChiliCheeseFries.cs
+++ Data/ChiliCheeseFries.cs
@@ -4,13 +4,20 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    public class ChiliCheeseFries
+    public class ChiliCheeseFries: Side
     {
         public double Price
         {
             get
             {
-
+                if(this.Size == Size.Small)
+                {
+                    return 1.99;
+                }
+                else if(this.Size == Size.Medium)
+                {
+                    return 2.99;
+                }
             }
         }
     }
