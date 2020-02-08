3d111b9ff1d63a623caa03c158e8ab08b88d246b
Easton Bolinger <bolinger@ksu.edu>
2020-02-08 14:27:51 -0600

    CornDodgers completed

---
+++ Data/CornDodgers.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// The class represeting the Corn Dodgers side 
+    /// </summary>
+    public class CornDodgers : Side
+    {
+        /// <summary>
+        /// The price of the corn dodgers
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                if(this.Size == Size.Small)
+                {
+                    return 1.59;
+                }
+                else if(this.Size == Size.Medium)
+                {
+                    return 1.79;
+                }
+                else
+                {
+                    return 1.99;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The calories of the corn dodgers 
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                if(this.Size == Size.Small)
+                {
+                    return 512;
+                }
+                else if(this.Size == Size.Medium)
+                {
+                    return 685;
+                }
+                else
+                {
+                    return 717;
+                }
+            }
+        }
+    }
+}
