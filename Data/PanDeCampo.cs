052fff7d6a24d987d6105afc14e0dddd276c47f8
Easton Bolinger <bolinger@ksu.edu>
2020-02-08 14:33:53 -0600

    PanDeCampo side finished

---
+++ Data/PanDeCampo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// The class representing the Pan de Campo side
+    /// </summary>
+    public class PanDeCampo : Side
+    {
+        /// <summary>
+        /// Price of the Pan de Campo side
+        /// </summary>
+        public override double Price
+        {
+            get
+            {
+                if(this.Size == Size.Small)
+                {
+                    return 1.59;
+                }
+                else if(this.Size == Size.Medium)
+                {
+                    return 1.79;
+                }
+                else
+                {
+                    return 1.99;
+                }
+            }
+        }
+
+        /// <summary>
+        /// The calories of the Pan de Campo side
+        /// </summary>
+        public override uint Calories
+        {
+            get
+            {
+                if(this.Size == Size.Small)
+                {
+                    return 227;
+                }
+                else if(this.Size == Size.Medium)
+                {
+                    return 269;
+                }
+                else
+                {
+                    return 367;
+                }
+            }
+        }
+    }
+}
