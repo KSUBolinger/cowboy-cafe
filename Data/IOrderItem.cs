ec3029aab8ed28ce59dd9edaa942b85380af8c64
Easton Bolinger <bolinger@ksu.edu>
2020-02-26 14:48:36 -0600

    Added orderTest.cs file

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// an interface representing a single item in an order
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// the price of this order
+        /// </summary>
+        double Price { get; }
+        /// <summary>
+        /// the special instructions for this order
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }
+}
---
+++ DataTests/UnitTests/OrderTests.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Author: Easton Bolinger
+ * Class: OrderTests.cs
+ * Purpose: to test the functionality of the order class
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CowboyCafe.Data;
+using Xunit;
+using System.Linq;
+
+namespace CowboyCafe.DataTests.UnitTests
+{
+    public class OrderTests
+    {
+        class MockOrderItem : IOrderItem
+        {
+            public double Price { get; set; }
+            public List<string> SpecialInstructions { get; set; }
+        }
+        [Fact]
+        //Adding something to the order should have it appear in the Items property
+        public void AddedIOrderItemAppearInItemsProperty()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            Assert.Contains(item, order.Items);
+        }
+
+        [Fact]
+        public void RemoveOrderItemDoesNotAppearInItemsProperty()
+        {
+            var order = new Order();
+            var item = new MockOrderItem();
+            order.Add(item);
+            order.Remove(item);
+            Assert.DoesNotContain(item, order.Items);
+        }
+
+        [Theory]
+        [InlineData(new double[] {0})]
+        [InlineData(new double[] { 10, 15, 18 })]
+        [InlineData(new double[] { 20, -4, 3.6, 8 })]
+        [InlineData(new double[] { -100, -5 })]
+        [InlineData(new double[] { })]
+        public void SubtotalShouldBeTheSumOfOrderItemPrices(double[] prices)
+        {
+            var order = new Order();
+            double total = 0;
+            foreach(var price in prices)
+            {
+                total += price;
+                order.Add(new MockOrderItem()
+                {
+                    Price = price
+                });
+            }
+            Assert.Equal(total, order.Subtotal);
+        }
+        //remove something from the order should remove it from the Items property 
+
+        //get the price - needs to be right (For the items we've added)
+        
+        [Fact]
+        public void ItemsShouldContainOnlyAddedItems()
+        {
+            var items = new IOrderItem[]
+            {
+                new MockOrderItem() {Price = 3 },
+                new MockOrderItem() {Price = 4 },
+                new MockOrderItem() {Price = 7 },
+            };
+            var order = new Order();
+            foreach(var item in items)
+            {
+                order.Add(item);
+            }
+            Assert.Equal(items.Length, order.Items.Count());
+
+            foreach(var item in items)
+            {
+                Assert.Contains(item, order.Items);
+            }
+        }
+    }
+}
